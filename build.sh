12e3b5452a37140917eb53474444428108316122
Nathan Heller <gritty.desk3330@fastmail.com>
2026-04-02 12:40:26 +0300

    internal links mostly working

--- build.sh
+++ build.sh
@@ -7,19 +7,31 @@ set -euo pipefail
 
 echo "---Starting build---"
 
+### FUNCTIONS ###
+
+function slugify() {
+    echo "$1" |
+    tr '[:upper:]' '[:lower:]' |
+    sed -E "s/['’]//g; s/[^a-z0-9]+/-/g; s/^-+|-+$//g"
+}
+
 ### VARIABLES ###
 
 BASE_DIR="$HOME/Projects/nate-spot-v3"
 
-CONTENT_DIR="$BASE_DIR/content"
+# CONTENT_DIR="$BASE_DIR/content"
+CONTENT_DIR="$HOME/Documents/Notes/Natespot"
 SITE_DIR="$BASE_DIR/site"
-TEMP_DIR="$BASE_DIR/temp"
 
 PAGES_DIR="$CONTENT_DIR/pages"
 POSTS_DIR="$CONTENT_DIR/posts"
 
+TEMP_DIR="$BASE_DIR/temp"
+PAGES_DIR_TEMP="$TEMP_DIR/pages"
+POSTS_DIR_TEMP="$TEMP_DIR/posts"
+
 INDEX_MD="$PAGES_DIR/index.md"
-INDEX_MD_TEMP="$TEMP_DIR/index.md"
+INDEX_MD_TEMP="$PAGES_DIR_TEMP/index.md"
 POST_LIST_MD_TEMP="$TEMP_DIR/post-list.md"
 
 ### SETUP ###
@@ -35,6 +47,9 @@ fi
 mkdir $SITE_DIR
 mkdir $TEMP_DIR
 
+mkdir $PAGES_DIR_TEMP
+mkdir $POSTS_DIR_TEMP
+
 cp -r "$BASE_DIR/styles" "$SITE_DIR"
 cp -r "$CONTENT_DIR/images" "$SITE_DIR"
 
@@ -43,52 +58,83 @@ cp $INDEX_MD $INDEX_MD_TEMP
 ### MAKE POSTS ###
 
 find $POSTS_DIR -type f ! -name "*.sync-conflict*" -name "*.md" | while read FILE; do
-    SLUG=$(basename "$FILE" .md)
-    OUTPUT_PATH="$SITE_DIR/$SLUG"
-    mkdir "$OUTPUT_PATH"
-    echo "Making post: $SLUG"
+    POST_TITLE=$(basename "$FILE" .md)
+    POST_SLUG_FALLBACK=$(slugify "$POST_TITLE") # Use slugified post title as fallback if no slug provided
+    POST_TEMP="$POSTS_DIR_TEMP/$POST_SLUG_FALLBACK.html"
+
+    echo "Making post: $POST_TITLE"
+
     {
-        read -r POST_TITLE;
+        read -r POST_SLUG;
         read -r POST_DATE;
     } < <(
-        pandoc "$FILE" -o "$OUTPUT_PATH/index.html" \
+        pandoc "$FILE" -o "$POST_TEMP" \
+            --metadata title="$POST_TITLE" \
             --template pandoc/base.html \
             --include-before-body pandoc/header.html \
             --lua-filter pandoc/meta.lua
     )
 
+    ### COPY TEMP POST TO SITE DIR ###
+
+    if [[ -z "$POST_SLUG" ]]; then
+        POST_SLUG="$POST_SLUG_FALLBACK"
+    fi
+
+    mkdir "$SITE_DIR/$POST_SLUG"
+    cat "$POST_TEMP" >> "$SITE_DIR/$POST_SLUG/index.html"
+
     ### ADD LINK TO POST LIST ###
 
     # POST_DATE_FORMATTED=$(date -d "$POST_DATE" "+%b %-d")
-    printf "%s\n" "- [$POST_DATE]{.post-date} [$POST_TITLE](/$SLUG)" >> $POST_LIST_MD_TEMP
+    printf "%s\n" "- [$POST_DATE]{.post-date} [$POST_TITLE](/$POST_SLUG)" >> $POST_LIST_MD_TEMP
 done
 
 ### ADD POST LIST TO INDEX ###
 
-printf "\n%s\n" "::: post-list" >> $INDEX_MD_TEMP
+printf "\n\n%s\n" "::: post-list" >> $INDEX_MD_TEMP
 cat $POST_LIST_MD_TEMP >> $INDEX_MD_TEMP
 printf "%s\n" ":::" >> $INDEX_MD_TEMP
 
 ### MAKE PAGES ###
 
 find $PAGES_DIR -type f ! -name "*.sync-conflict*" -name "*.md" | while read FILE; do
-    SLUG=$(basename "$FILE" .md)
-    OUTPUT_PATH="$SITE_DIR/$SLUG"
-    INPUT_FILE="$FILE"
+    PAGE_TITLE=$(basename "$FILE" .md)
+    PAGE_SLUG_FALLBACK=$(slugify "$PAGE_TITLE") # Use slugified page title as fallback if no slug provided
+    PAGE_TEMP="$PAGES_DIR_TEMP/$PAGE_SLUG_FALLBACK.html"
 
-    echo "Making page: $SLUG"
+    echo "Making page: $PAGE_TITLE"
 
-    if [ $SLUG == "index" ]; then
+    ### HANDLE INDEX PAGE SEPARATELY ###
+
+    if [[ $PAGE_TITLE == "index" ]]; then
         INPUT_FILE="$INDEX_MD_TEMP"
-        OUTPUT_FILE="$OUTPUT_PATH.html"
-    else
-        mkdir "$OUTPUT_PATH"
-        OUTPUT_FILE="$OUTPUT_PATH/index.html"
-    fi
+        OUTPUT_FILE="$SITE_DIR/$PAGE_TITLE.html"
 
-    pandoc "$INPUT_FILE" -o "$OUTPUT_FILE" \
-        --template pandoc/base.html \
-        --include-before-body pandoc/header.html
+        pandoc "$INPUT_FILE" -o "$OUTPUT_FILE" \
+            --template pandoc/base.html \
+            --include-before-body pandoc/header.html \
+            --lua-filter pandoc/meta.lua
+    else
+        {
+            read -r PAGE_SLUG;
+        } < <(
+            pandoc "$FILE" -o "$PAGE_TEMP" \
+                --metadata title="$PAGE_TITLE" \
+                --template pandoc/base.html \
+                --include-before-body pandoc/header.html \
+                --lua-filter pandoc/meta.lua
+        )
+
+        ### COPY TEMP PAGE TO SITE DIR ###
+
+        if [[ -z "$PAGE_SLUG" ]]; then
+            PAGE_SLUG = $PAGE_SLUG_FALLBACK
+        fi
+
+        mkdir "$SITE_DIR/$PAGE_SLUG"
+        cat "$PAGE_TEMP" >> "$SITE_DIR/$PAGE_SLUG/index.html"
+    fi
 done
 
 ### CLEANUP ###
--- pandoc/meta.lua
+++ pandoc/meta.lua
@@ -1,20 +1,107 @@
 function Meta(meta)
-    if meta.title then
-        local title_string = pandoc.utils.stringify(meta.title)
-        io.write(title_string .. "\n")
+    -- Skip index (the only page without a title)
+    if not meta.title then
+        return meta
+    end
+
+    local slug_string = ""
+    local date_string = ""
+
+    if meta.slug then
+        slug_string = pandoc.utils.stringify(meta.slug)
     end
 
     if meta.date then
-        local date_string = pandoc.utils.stringify(meta.date)
-        local y, m, d = date_string:match("(%d+)-(%d+)-(%d+)")
+        local meta_date_string = pandoc.utils.stringify(meta.date)
+        local y, m, d = meta_date_string:match("(%d+)-(%d+)-(%d+)")
         local ts = os.time({ year = y, month = m, day = d })
 
         local formatted = os.date("%b %d", ts)
         local formatted_w_year = os.date("%b %d, %Y", ts)
 
         meta.date = formatted_w_year
-        io.write(formatted .. "\n")
+        date_string = formatted
     end
 
+    io.write(slug_string .. "\n")
+    io.write(date_string .. "\n")
     return meta
+end
+
+-- local path_utils = require 'pandoc.path'
+
+function Link(el)
+    -- If external link, open in new window and add arrow symbol
+    -- Else if internal link, use slug from target file
+    if string.find(el.target, "^http") then
+        el.attributes.target = "_blank"
+        el.content = pandoc.utils.stringify(el.content) .. "\u{2197}\u{fe0e}"
+    else
+        -- If link target is missing file extension, it's already a slug and can be skipped
+        if not string.find(el.target, ".md") then
+            return el
+        end
+
+        local home_dir = os.getenv("HOME")
+        local content_dir = home_dir .. "/Documents/Notes/Natespot"
+
+        local pages_subdir = "/pages/"
+        local posts_subdir = "/posts/"
+
+        -- Set target to slugified title as fallback if meta.slug is not found
+        local target_link_filename = get_filename_from_path(el.target)
+        local target_link_slug = "/" .. slugify(target_link_filename)
+
+        local input_file = PANDOC_STATE.input_files[1]
+        local target_link_path_rel = el.target:gsub("%%20", " ")
+        local target_file_path
+
+        if string.find(input_file, pages_subdir) then
+            target_file_path = content_dir .. pages_subdir .. target_link_path_rel
+        elseif string.find(input_file, posts_subdir) then
+            target_file_path = content_dir .. posts_subdir .. target_link_path_rel
+        end
+
+        if target_file_path then
+            target_file_path = target_file_path:gsub("[/\\]+", "/") -- Remove any double slashes
+
+            local target_file, err = io.open(target_file_path, "r")
+
+            if err then
+                print("ERROR: ", err)
+            end
+
+            local file_content = target_file:read("*all")
+            local file_meta = pandoc.read(file_content).meta
+
+            if file_meta.slug then
+                target_link_slug = "/" .. pandoc.utils.stringify(file_meta.slug)
+            end
+
+            target_file:close()
+            -- else
+            --     print(err)
+        end
+
+        el.target = target_link_slug
+    end
+
+    return el
+end
+
+function slugify(str)
+    str = str:lower()
+    str = str:gsub("['’]", "") -- remove apostrophes
+    str = str:gsub("[^a-z0-9]+", "-") -- replace non-alphanumerics with hyphens
+    str = str:gsub("^-+", ""):gsub("-+$", "") -- trim leading/trailing hyphens
+
+    return str
+end
+
+function get_filename_from_path(path)
+    local filename = pandoc.path.filename(path)
+    local filename_no_ext = pandoc.path.split_extension(filename)
+    local filename_with_spaces = filename:gsub("%%20", " ")
+
+    return filename_with_spaces
 end
